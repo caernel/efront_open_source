cea0db21eba6b215a171b066a3071bed31f172d9
Periklis Venakis <pvenakis@efrontlearning.net>
2013-07-03 11:59:48 +0300

    sync with source

--- www/periodic_updater.php
+++ www/periodic_updater.php
@@ -34,29 +34,28 @@ try {
 	$newTime = '';
 	$jsonValues = array();
 	if ($_SESSION['s_login']) {
-		if (empty($_SESSION['last_periodic_check']) || time() - $_SESSION['last_periodic_check'] >= ceil($GLOBALS['configuration']['updater_period']/1000)) {
-			$_SESSION['last_periodic_check'] = time();
-			$entity = getUserTimeTarget($_GET['HTTP_REFERER']);
-			//Update times for this entity
-			if ($_SESSION['s_lesson_user_type'] == 'student' && isset($_POST['user_total_time_in_unit']) && current($entity) == 'unit' && eF_checkParameter(key($entity), 'id')) {
-				$newTime = $_POST['user_total_time_in_unit'];
-				$jsonValues['entity'] = current($entity);
-				$jsonValues['entity_id'] = current($entity);
-				if ($newTime && is_numeric($newTime)) {
-					$result = eF_executeNew("insert into users_to_content (users_LOGIN, content_ID, lessons_ID) values('".$_SESSION['s_login']."', ".key($entity).", ".$_SESSION['s_lessons_ID'].") on duplicate key update total_time=$newTime");
-					$jsonValues['time_in_unit'] = EfrontTimes::formatTimeForReporting($newTime);
-				} else {
-					$jsonValues['old_time_in_unit'] = EfrontTimes::formatTimeForReporting((EfrontLesson::getUserActiveTimeInUnit($_SESSION['s_login'], key($entity))));
-					$jsonValues['old_time_in_lesson'] = EfrontTimes::formatTimeForReporting((EfrontLesson::getUserActiveTimeInLesson($_SESSION['s_login'], $_SESSION['s_lessons_ID'])));
-				}
+		$entity = getUserTimeTarget($_GET['HTTP_REFERER']);
+		//Update times for this entity
+		if ($_SESSION['s_lesson_user_type'] == 'student' && isset($_POST['user_total_time_in_unit']) && current($entity) == 'unit' && eF_checkParameter(key($entity), 'id')) {
+			$newTime = $_POST['user_total_time_in_unit'];
+			$jsonValues['entity'] = current($entity);
+			$jsonValues['entity_id'] = current($entity);
+			if ($newTime && is_numeric($newTime)) {
+				$result = eF_executeNew("insert into users_to_content (users_LOGIN, content_ID, lessons_ID) values('".$_SESSION['s_login']."', ".key($entity).", ".$_SESSION['s_lessons_ID'].") on duplicate key update total_time=$newTime");
+				$jsonValues['time_in_unit'] = EfrontTimes::formatTimeForReporting($newTime);
+			} else {
+				$jsonValues['old_time_in_unit'] = EfrontTimes::formatTimeForReporting((EfrontLesson::getUserActiveTimeInUnit($_SESSION['s_login'], key($entity))));
+				$jsonValues['old_time_in_lesson'] = EfrontTimes::formatTimeForReporting((EfrontLesson::getUserActiveTimeInLesson($_SESSION['s_login'], $_SESSION['s_lessons_ID'])));
 			}
+		}
 
 
-		
+		if (empty($_SESSION['last_periodic_check']) || time() - $_SESSION['last_periodic_check'] >= ceil($GLOBALS['configuration']['updater_period']/1000)) {
 			$result = eF_executeNew("update user_times set time=time+(".time()."-timestamp_now),timestamp_now=".time()."
 					where session_expired = 0 and session_custom_identifier = '".$_SESSION['s_custom_identifier']."' and users_LOGIN = '".$_SESSION['s_login']."'
 					and entity = '".current($entity)."' and entity_id = '".key($entity)."'");
 			
+			$_SESSION['last_periodic_check'] = time();
 			$jsonValues['online'] = EfrontUser :: getUsersOnline($GLOBALS['configuration']['autologout_time'] * 60);
 			$messages = eF_getTableData("f_personal_messages pm, f_folders ff", "count(*)", "pm.users_LOGIN='".$_SESSION['s_login']."' and viewed='no' and f_folders_ID=ff.id and ff.name='Incoming'");
 			$jsonValues['messages'] = $messages[0]['count(*)'];
